9d33ff3a9b8900f508591f44d5010b6c537e0a65
jerard <qbin221@gmail.com>
2020-03-17 15:41:33 -0400

    added getSavedBooks function shortcut

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default {
   getBooks: function(q) {
     return axios.get("/api/google", { params: { q: "title:" + q } });
-  }
+  },
+  getSavedBooks: function() {
+    return axios.get("/api/books");
+  },
 }
